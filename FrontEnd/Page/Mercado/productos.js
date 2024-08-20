e2b88692239ae25c54c19b0dc1301188e120ad8d
DESKTOP-BKD77DO\ADMIN <Dremyon@gmail.com>
2024-08-20 16:14:42 -0300

    .

--- FrontEnd/Page/Mercado/productos.js
+++ FrontEnd/Page/Mercado/productos.js
@@ -1,9 +1,30 @@
-window.onload = async () => {
+window.onload= async()=>{
+   // let productos = await new MercadoController().obtenerProducto();
    let productos = await obtenerProducto();
    mostrarProductos(productos);
+ }
+
+ function mostrarProductos(productos){
+    let thProductoElement = document.querySelector("#tableProductos");
+    productos.forEach(producto => {
+       let filaProducto = document.createElement("tr");
+       filaProducto.innerHTML = `
+                    <td><p>${producto.title}</p></td>
+                    <td><a href="${producto.permalink}" target="_blank">Ver en mercado libre</a></td>
+                    <td><img src="${producto.thumbnail}" alt="ERROR"></td>
+                    <td><p>$ ${producto.price}</p></td>
+       `;
+       let botonProducto = document.createElement("button");
+       filaProducto.appendChild(botonProducto);
+       botonProducto.innerHTML = "Guardar"
+       botonProducto.onclick = () =>{
+         guardarProducto(producto);
+       }
+       thProductoElement.appendChild(filaProducto);
+      });
 }
 
-async function obtenerProducto() {
+async function obtenerProducto(){
    let url = "https://api.mercadolibre.com/sites/MLU/search?category=MLU1144";
    let consulta = await fetch(url);
    let datos = await consulta.json();
@@ -12,50 +33,25 @@ async function obtenerProducto() {
    return productos;
 }
 
-function mostrarProductos(productos) {
-   let thProductoElement = document.querySelector(".tbodyProducto");
-   productos.forEach(producto => {
-       let filaProducto = document.createElement("tr");
-       filaProducto.innerHTML = `
-           <td><p>${producto.title}</p></td>
-           <td><a href="${producto.permalink}" target="_blank">Ver en mercado libre</a></td>
-           <td><img src="${producto.thumbnail}" alt="ERROR"></td>
-           <td><p>$${producto.price}</p></td>
-       `;
-       let tdBtn = document.createElement("td");
-       let btnFila = document.createElement("button");
-       tdBtn.appendChild(btnFila);
-       filaProducto.appendChild(tdBtn);
-       btnFila.innerHTML = "Guardar";
-       btnFila.onclick = () => {
-           guardarProducto(producto);
-       }
-       thProductoElement.appendChild(filaProducto);
-   });
-}
 
-async function guardarProducto(producto) {
+async function guardarProducto(producto){
    let dataProducto = new FormData();
-   dataProducto.append("titulo", producto.title);
-   dataProducto.append("permalink", producto.permalink);
-   dataProducto.append("tumbnail", producto.thumbnail);
-   dataProducto.append("precio", producto.price);
-   dataProducto.append("id", producto.catalog_product_id);
-   
-   // Ajusta la URL para que coincida con la estructura de tu proyecto
-   let url = window.location.origin + "/BackEnd/Controllers/MercadoController.php?function=agregar";
-   
+   dataProducto.append('id', producto.id)
+   dataProducto.append('titulo', producto.title);
+   dataProducto.append('permalink', producto.permalink);
+   dataProducto.append('thumbnail', producto.thumbnail);
+   dataProducto.append('precio', producto.price);
+   let url = window.location.origin + "/API-MercadoLibre-Pesuuti/backEnd/controller/MercadoController.php?funcion=agregar";
    let config = {
-       method: 'POST',
+       method: 'POST' ,
        body: dataProducto
    }
-   
    let respuesta = await fetch(url, config);
    let datosRespuesta = await respuesta.json();
-   
-   if (datosRespuesta) {
-       alert("Producto agregado con éxito");
-   } else {
+   if(datosRespuesta){
+       alert("Producto agregado con exito");
+   }
+   else{
        alert("Error al agregar el producto");
    }
 }
